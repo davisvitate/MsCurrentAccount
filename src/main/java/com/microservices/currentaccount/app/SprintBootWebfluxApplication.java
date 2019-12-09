7e40d3205bb1fde8b956674861e993a8b482f43b
davisvitate <davisvitate@gmail.com>
2019-12-09 13:10:02 -0500

    version 1

---
+++ src/main/java/com/microservices/currentaccount/app/SprintBootWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.currentaccount.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootWebfluxApplication.class, args);
+	}
+
+}
